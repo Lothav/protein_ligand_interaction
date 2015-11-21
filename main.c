4710752cc91ba344d75d129b068f87ccf651558c
Luiz0tavio <luizotavioresende@hotmail.com>
2015-11-21 19:22:58 -0200

    get cube coords

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 /* 
  * File:   main.c
- * Author: root
+ * Author: LuizOtavio
  *
  * Created on 21 de Novembro de 2015, 01:27
  */
@@ -14,11 +14,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define MAX 50
 
+typedef struct cube_str{
+    double max_x, max_y, max_z;
+    double min_x, min_y, min_z;
+    double edge;
+}Cube;
+
+
+void setCubeCoords(Cube *cube){
+      
+    char str[MAX];
+    
+    // seta o valor MÍNIMO de x,y,z para o cubo
+    fgets(str, sizeof (str), stdin);
+    sscanf(str, " %lf %lf %lf", &cube->min_x, &cube->min_y,&cube->min_z);
+    
+    // seta o valor MÁXIMO de x,y,z para o cubo
+    fgets(str, sizeof (str), stdin);
+    sscanf(str, " %lf %lf %lf", &cube->max_x, &cube->max_y, &cube->max_z);
+}
+
+int main(int argc, char** argv) {
+    
+    double cubeLig_edge;
+    char str[MAX], lig_name[MAX];
+    Cube *out_cube;
+    int a;
+    
+    out_cube = (Cube *) malloc( sizeof(Cube) );
+    
+    // armazena o valor da aresta do cubo em volta de cada ligante
+    fgets(str, sizeof (str), stdin);
+    sscanf(str, "%lf", &cubeLig_edge);
+    
+    // recebe o nome do ligante
+    fgets(str, sizeof (str), stdin);
+    sscanf(str, "%*[^0-9]%s", lig_name);
+    
+    setCubeCoords(out_cube);
+    
     return (EXIT_SUCCESS);
 }
 
